cf93d0ab607a34fd78830c7f2d74dadce90bb3c3
Bobomurod <Bobomurod@DESKTOP-DEM029F>
2024-06-08 10:45:13 +0500

    second step

--- QuizApp/Form5.Designer.cs
+++ QuizApp/Form5.Designer.cs
@@ -30,12 +30,26 @@
         {
             label1 = new Label();
             textBox1 = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            checkBox1 = new CheckBox();
+            checkBox2 = new CheckBox();
+            checkBox3 = new CheckBox();
+            checkBox4 = new CheckBox();
+            button1 = new Button();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.ForeColor = SystemColors.ControlText;
             label1.Location = new Point(51, 50);
             label1.Name = "label1";
             label1.Size = new Size(69, 28);
@@ -44,16 +58,153 @@
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(170, 51);
+            textBox1.Location = new Point(161, 54);
+            textBox1.Multiline = true;
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(125, 27);
+            textBox1.Size = new Size(294, 28);
             textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(51, 111);
+            label2.Name = "label2";
+            label2.Size = new Size(95, 28);
+            label2.TabIndex = 2;
+            label2.Text = "A varyant";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(51, 171);
+            label3.Name = "label3";
+            label3.Size = new Size(93, 28);
+            label3.TabIndex = 3;
+            label3.Text = "B varyant";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.Location = new Point(53, 229);
+            label4.Name = "label4";
+            label4.Size = new Size(94, 28);
+            label4.TabIndex = 4;
+            label4.Text = "C varyant";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label5.Location = new Point(53, 294);
+            label5.Name = "label5";
+            label5.Size = new Size(96, 28);
+            label5.TabIndex = 5;
+            label5.Text = "D varyant";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(161, 111);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(294, 28);
+            textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(161, 171);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(294, 28);
+            textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(161, 229);
+            textBox4.Multiline = true;
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(294, 28);
+            textBox4.TabIndex = 8;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(161, 294);
+            textBox5.Multiline = true;
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(294, 28);
+            textBox5.TabIndex = 9;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(483, 113);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(109, 24);
+            checkBox1.TabIndex = 10;
+            checkBox1.Text = "To'gri javob";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Location = new Point(483, 173);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(109, 24);
+            checkBox2.TabIndex = 11;
+            checkBox2.Text = "To'gri javob";
+            checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            checkBox3.AutoSize = true;
+            checkBox3.Location = new Point(483, 231);
+            checkBox3.Name = "checkBox3";
+            checkBox3.Size = new Size(109, 24);
+            checkBox3.TabIndex = 12;
+            checkBox3.Text = "To'gri javob";
+            checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            checkBox4.AutoSize = true;
+            checkBox4.Location = new Point(483, 296);
+            checkBox4.Name = "checkBox4";
+            checkBox4.Size = new Size(109, 24);
+            checkBox4.TabIndex = 13;
+            checkBox4.Text = "To'gri javob";
+            checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(161, 361);
+            button1.Name = "button1";
+            button1.Size = new Size(198, 48);
+            button1.TabIndex = 14;
+            button1.Text = "Saqlash";
+            button1.UseVisualStyleBackColor = true;
             // 
             // Form5
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlLight;
             ClientSize = new Size(800, 641);
+            Controls.Add(button1);
+            Controls.Add(checkBox4);
+            Controls.Add(checkBox3);
+            Controls.Add(checkBox2);
+            Controls.Add(checkBox1);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
             Controls.Add(textBox1);
             Controls.Add(label1);
             Name = "Form5";
@@ -66,5 +217,18 @@
 
         private Label label1;
         private TextBox textBox1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private CheckBox checkBox1;
+        private CheckBox checkBox2;
+        private CheckBox checkBox3;
+        private CheckBox checkBox4;
+        private Button button1;
     }
 }
