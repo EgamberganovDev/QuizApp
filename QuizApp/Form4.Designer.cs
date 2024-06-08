23a5153937f04fb411dd1b794ffd4c354dc0ba9b
Bobomurod <Bobomurod@DESKTOP-DEM029F>
2024-06-08 10:12:42 +0500

    first step

--- QuizApp/Form4.Designer.cs
+++ QuizApp/Form4.Designer.cs
@@ -28,73 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            label1 = new Label();
-            label2 = new Label();
-            label3 = new Label();
-            label4 = new Label();
-            label5 = new Label();
-            label6 = new Label();
             SuspendLayout();
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
-            label1.Location = new Point(63, 79);
-            label1.Name = "label1";
-            label1.Size = new Size(90, 37);
-            label1.TabIndex = 0;
-            label1.Text = "label1";
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Font = new Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
-            label2.Location = new Point(63, 164);
-            label2.Name = "label2";
-            label2.Size = new Size(90, 37);
-            label2.TabIndex = 1;
-            label2.Text = "label2";
-            // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.Font = new Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
-            label3.Location = new Point(63, 251);
-            label3.Name = "label3";
-            label3.Size = new Size(90, 37);
-            label3.TabIndex = 2;
-            label3.Text = "label3";
-            // 
-            // label4
-            // 
-            label4.AutoSize = true;
-            label4.Font = new Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
-            label4.Location = new Point(63, 340);
-            label4.Name = "label4";
-            label4.Size = new Size(90, 37);
-            label4.TabIndex = 3;
-            label4.Text = "label4";
-            // 
-            // label5
-            // 
-            label5.AutoSize = true;
-            label5.Font = new Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
-            label5.Location = new Point(63, 411);
-            label5.Name = "label5";
-            label5.Size = new Size(90, 37);
-            label5.TabIndex = 4;
-            label5.Text = "label5";
-            // 
-            // label6
-            // 
-            label6.AutoSize = true;
-            label6.Font = new Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
-            label6.Location = new Point(313, 411);
-            label6.Name = "label6";
-            label6.Size = new Size(90, 37);
-            label6.TabIndex = 5;
-            label6.Text = "label6";
             // 
             // Form4
             // 
@@ -108,12 +42,5 @@
         }
 
         #endregion
-
-        private Label label1;
-        private Label label2;
-        private Label label3;
-        private Label label4;
-        private Label label5;
-        private Label label6;
     }
 }
