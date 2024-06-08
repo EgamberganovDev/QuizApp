6b803beba47de3f48cba334c2eac34b6bda7154c
Bobomurod <Bobomurod@DESKTOP-DEM029F>
2024-06-08 10:28:58 +0500

    first step

--- QuizApp/Form4.Designer.cs
+++ QuizApp/Form4.Designer.cs
@@ -39,36 +39,36 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(32, 52);
+            label1.Location = new Point(37, 69);
             label1.Name = "label1";
-            label1.Size = new Size(38, 15);
+            label1.Size = new Size(50, 20);
             label1.TabIndex = 0;
             label1.Text = "label1";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(32, 124);
+            label2.Location = new Point(37, 165);
             label2.Name = "label2";
-            label2.Size = new Size(38, 15);
+            label2.Size = new Size(50, 20);
             label2.TabIndex = 1;
             label2.Text = "label2";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(32, 182);
+            label3.Location = new Point(37, 243);
             label3.Name = "label3";
-            label3.Size = new Size(38, 15);
+            label3.Size = new Size(50, 20);
             label3.TabIndex = 2;
             label3.Text = "label3";
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(32, 238);
+            label4.Location = new Point(37, 317);
             label4.Name = "label4";
-            label4.Size = new Size(38, 15);
+            label4.Size = new Size(50, 20);
             label4.TabIndex = 3;
             label4.Text = "label4";
             label4.Click += label4_Click;
@@ -76,34 +76,33 @@
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(32, 299);
+            label5.Location = new Point(37, 399);
             label5.Name = "label5";
-            label5.Size = new Size(38, 15);
+            label5.Size = new Size(50, 20);
             label5.TabIndex = 4;
             label5.Text = "label5";
             // 
             // label6
             // 
             label6.AutoSize = true;
-            label6.Location = new Point(207, 299);
+            label6.Location = new Point(237, 399);
             label6.Name = "label6";
-            label6.Size = new Size(38, 15);
+            label6.Size = new Size(50, 20);
             label6.TabIndex = 5;
             label6.Text = "label6";
             // 
             // Form4
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = SystemColors.InactiveCaption;
-            ClientSize = new Size(278, 349);
+            BackColor = SystemColors.ControlLightLight;
+            ClientSize = new Size(318, 465);
             Controls.Add(label6);
             Controls.Add(label5);
             Controls.Add(label4);
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
-            Margin = new Padding(3, 2, 3, 2);
             Name = "Form4";
             Text = "Form4";
             Load += Form4_Load;
