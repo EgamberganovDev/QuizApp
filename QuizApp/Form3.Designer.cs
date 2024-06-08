e25eb35c013a8c59ee4e5207192dad6c0622c9af
Bobomurod <Bobomurod@DESKTOP-DEM029F>
2024-06-08 09:46:58 +0500

    first step

--- QuizApp/Form3.Designer.cs
+++ QuizApp/Form3.Designer.cs
@@ -28,7 +28,118 @@
         /// </summary>
         private void InitializeComponent()
         {
+            listBox1 = new ListBox();
+            label1 = new Label();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            radioButton3 = new RadioButton();
+            radioButton4 = new RadioButton();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
             SuspendLayout();
+            // 
+            // listBox1
+            // 
+            listBox1.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 28;
+            listBox1.Location = new Point(61, 83);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(942, 88);
+            listBox1.TabIndex = 0;
+            listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(61, 39);
+            label1.Name = "label1";
+            label1.Size = new Size(76, 28);
+            label1.TabIndex = 1;
+            label1.Text = "Savol 1";
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            radioButton1.Location = new Point(105, 226);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(97, 32);
+            radioButton1.TabIndex = 2;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "Javob 1";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            radioButton2.Location = new Point(105, 297);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(97, 32);
+            radioButton2.TabIndex = 3;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "Javob 2";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            radioButton3.AutoSize = true;
+            radioButton3.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            radioButton3.Location = new Point(105, 368);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new Size(97, 32);
+            radioButton3.TabIndex = 4;
+            radioButton3.TabStop = true;
+            radioButton3.Text = "Javob 3";
+            radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            radioButton4.AutoSize = true;
+            radioButton4.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            radioButton4.Location = new Point(105, 443);
+            radioButton4.Name = "radioButton4";
+            radioButton4.Size = new Size(97, 32);
+            radioButton4.TabIndex = 5;
+            radioButton4.TabStop = true;
+            radioButton4.Text = "Javob 4";
+            radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.Location = new Point(192, 498);
+            button1.Name = "button1";
+            button1.Size = new Size(130, 48);
+            button1.TabIndex = 6;
+            button1.Text = "Oldingi";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            button2.Location = new Point(382, 498);
+            button2.Name = "button2";
+            button2.Size = new Size(130, 48);
+            button2.TabIndex = 7;
+            button2.Text = "Keyingi";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            button3.Location = new Point(727, 498);
+            button3.Name = "button3";
+            button3.Size = new Size(225, 48);
+            button3.TabIndex = 8;
+            button3.Text = "Testni yakunlash";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
             // 
             // Form3
             // 
@@ -42,5 +153,14 @@
         }
 
         #endregion
+        private ListBox listBox1;
+        private Label label1;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private RadioButton radioButton3;
+        private RadioButton radioButton4;
+        private Button button1;
+        private Button button2;
+        private Button button3;
     }
 }
---
+++ QuizApp/Form4.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuizApp
+{
+    public partial class Form4 : Form
+    {
+        public Form4(string IsmFamiliya, string FanNomi, int TestlarSoni, int cnt)
+        {
+            InitializeComponent();
+        }
+
+        private void Form4_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
